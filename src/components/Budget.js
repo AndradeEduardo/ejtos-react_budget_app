dae3f0026cd23be191b84557251c0847aba7f9c7
AndradeEduardo <dudsdu@gmail.com>
2023-06-20 03:22:44 -0300

    fim da prática

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,13 @@
-
+import React, { useContext } from 'react'; //This is the hook
+// React is the defaul export from react; useContext is a named export from react
+import { AppContext } from '../context/AppContext'; //This is the app context
+const Budget = () => {
+    const { budget, currency } = useContext(AppContext);//pegando dois objetos do AppContext
+    // currenncy já foi enganchada por mim
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency}{budget}</span>
+        </div>
+    );
+};
+export default Budget;
