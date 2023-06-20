4dd3c23ba7bebf877c6d0085ff634df896a63782
AndradeEduardo <dudsdu@gmail.com>
2023-06-20 04:18:34 -0300

    comments added

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,8 +1,8 @@
 
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
-const Remaining = () => {
-    const { expenses, budget, currency } = useContext(AppContext);
+import React, { useContext } from 'react'; //importa o hook
+import { AppContext } from '../context/AppContext'; //importa o AppContext
+const Remaining = () => { // define uma função sem argumentos
+    const { expenses, budget, currency } = useContext(AppContext); // pega os valores das 3 variáveis
     const totalExpenses = expenses.reduce((total, item) => {
         return (total = total + item.cost);
     }, 0);
